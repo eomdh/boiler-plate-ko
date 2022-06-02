470b429c194c631675aa371ecadfce39f4f79775
eomdh <edh9692@gmail.com>
2022-06-02 20:05:15 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
+
+const config = require('./config/key')
+
+const { User } = require("./models/User")
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+// application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://eomdh:qwer1234@boilerplate.bj73qtv.mongodb.net/?retryWrites=true&w=majority', {})
+mongoose.connect(config.mongoURI, {})
   .then(() => console.log('MongoDB Connected..'))
   .catch(err => console.log(err))
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
+app.post('/register', (req, res) => {
+  // 회원가입에 필요한 정보를 client에서 가져오면
+  // 그것들을 DB에 넣어줌
+
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success: false, err })
+    return res.status(200).json({
+      success: true
+    })
+  })
+})
+ 
 app.listen(port, () => console.log(`listening on port ${port}!`))
